7b8fa8a0165d76f917650e2527f84219bf51902e
JayAyAre <sandyjordi9@gmail.com>
2023-06-09 14:53:17 +0200

    grpc_proxy_v2

--- Proxy-terminal/grpc_proxy.py
+++ Proxy-terminal/grpc_proxy.py
@@ -10,8 +10,12 @@ channel = grpc.insecure_channel('localhost:50051')
 stub = terminal_pb2_grpc.send_resultsStub(channel)
 r = redis.Redis(host='localhost', port=6379, decode_responses=False)
 first = False
+first_2 = False
 threshold = 0
-
+p_last = dict()
+w_last = dict()
+p_last2 = []
+w_last2 = []
 # create a valid request message
 while True:
     timer = Timestamp()
@@ -31,30 +35,93 @@ while True:
     else:
         threshold+=1
 
+    if first_2 == False:
+        for x in pollution_dict.keys():
+            for y in pollution_dict[x]:
+                p_last2.append({'id':y['id'],'timer_seconds':y['timer_seconds'],'value':y['value']})
+                p_last[str(y["id"])]=p_last2
+    if first_2 == False:
+        for x in wellness_dict.keys():
+            for y in wellness_dict[x]:
+                w_last2.append({'id':y['id'],'timer_seconds':y['timer_seconds'],'value':y['value']})
+                w_last[str(y["id"])]=w_last2
+
     for x in pollution_dict.keys():
         for y in pollution_dict[x]:
-            if threshold==0:
-                y["timer_seconds"]= 0
+            if first_2 == True:
+                for z in p_last.keys():
+                    for w in p_last[z]:
+                        if w['id']!=y['id'] and w['timer_seconds']!=y['timer_seconds'] and w['value']!=y['value']:
+                            if threshold == 0:
+                                y["timer_seconds"] = 0
+                            else:
+                                y["timer_seconds"] = threshold
+                            p1.append(
+                                terminal_pb2.pollutionData(id=y['id'], timestamp=timer.FromSeconds(y['timer_seconds']),
+                                                       coefficient=y['value']))
+                        else:
+                            if threshold == 0:
+                                y["timer_seconds"] = 0
+                            else:
+                                y["timer_seconds"] = threshold
             else:
-                y["timer_seconds"]= threshold
+                if threshold == 0:
+                    y["timer_seconds"] = 0
+                else:
+                    y["timer_seconds"] = threshold
 
     for x in wellness_dict.keys():
         for y in wellness_dict[x]:
-            if threshold==0:
-                y["timer_seconds"]= 0
+            if first_2 == True:
+                for z in w_last.keys():
+                    for w in w_last[z]:
+                        if w['id']!=y['id'] and w['timer_seconds']!=y['timer_seconds'] and w['value']!=y['value']:
+                            if threshold == 0:
+                                y["timer_seconds"] = 0
+                            else:
+                                y["timer_seconds"] = threshold
+                            w1.append(
+                                terminal_pb2.wellnessData(id=y['id'], timestamp=timer.FromSeconds(y['timer_seconds']),
+                                                       coefficient=y['value']))
+                        else:
+                            if threshold == 0:
+                                y["timer_seconds"] = 0
+                            else:
+                                y["timer_seconds"] = threshold
             else:
-                y["timer_seconds"]= threshold
+                if threshold == 0:
+                    y["timer_seconds"] = 0
+                else:
+                    y["timer_seconds"] = threshold
+
+    #for x in pollution_dict.keys():
+    #    for y in pollution_dict[x]:
+    #        if threshold==0:
+    #            y["timer_seconds"]= 0
+    #        else:
+    #            y["timer_seconds"]= threshold
+
+    #for x in wellness_dict.keys():
+    #    for y in wellness_dict[x]:
+    #        if threshold==0:
+    #            y["timer_seconds"]= 0
+    #        else:
+    #           y["timer_seconds"]= threshold
 
     p1 = []
     w1 = []
-    for x in pollution_dict.keys():
-        for y in pollution_dict[x]:
-            p1.append(terminal_pb2.pollutionData(id=y['id'], timestamp=timer.FromSeconds(y['timer_seconds']), coefficient=y['value']))
 
-    for x in wellness_dict.keys():
-        for y in wellness_dict[x]:
-            w1.append(terminal_pb2.wellnessData(id=y['id'], timestamp=timer.FromSeconds(y['timer_seconds']), coefficient=y['value']))
 
+    if first_2==False:
+        for x in pollution_dict.keys():
+            for y in pollution_dict[x]:
+                p1.append(terminal_pb2.pollutionData(id=y['id'], timestamp=timer.FromSeconds(y['timer_seconds']), coefficient=y['value']))
+        for x in wellness_dict.keys():
+            for y in wellness_dict[x]:
+                w1.append(terminal_pb2.wellnessData(id=y['id'], timestamp=timer.FromSeconds(y['timer_seconds']), coefficient=y['value']))
+
+    print(first_2)
+    first_2=True
     #data = terminal_pb2.airData(pollution=p1, wellness=w1)
     #stub.send_results(data)
     time.sleep(2)
